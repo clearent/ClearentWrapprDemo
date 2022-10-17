7cd163da1a38b98922dbceb582f3a679a4855ed5
adrianrusu16 <72539388+adrianrusu16@users.noreply.github.com>
2022-10-17 17:11:32 +0300

    hid keys

--- app/src/main/java/com/example/clearentwrapperdemo/Constants.java
+++ app/src/main/java/com/example/clearentwrapperdemo/Constants.java
@@ -3,6 +3,6 @@ package com.example.clearentwrapperdemo;
 public final class Constants {
 
     public static final String BASE_URL_SANDBOX = "https://gateway-sb.clearent.net";
-    public static final String PUBLIC_KEY_SANDBOX = "307a301406072a8648ce3d020106092b240303020801010c036200041fcefcdf366991b57f0fccf9efd587d0eee8d8ef8e5c78c17c2766d17a3b44b52bd999da8873e4daa144d76159d98a7f0fd94b65c49580ce134899f3826cd98380927c42fceec6e183a5ed4a064b43fef8507984ac855ca92b0ce32c50264670";
-    public static final String API_KEY_SANDBOX = "27a419e3ecad4d58aa6009b65e66fd81";
+    public static final String PUBLIC_KEY_SANDBOX = BuildConfig.PUBLIC_KEY;
+    public static final String API_KEY_SANDBOX = BuildConfig.API_KEY;
 }
